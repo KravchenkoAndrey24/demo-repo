cae809311dbb9f711e3f2194c77df4a051ddeaed
Andrii Kravchenko <yazaandrey@gmail.com>
2024-04-26 11:47:59 +0200

    fix: test redeploy on vercel

--- src/modules/order/pages/Orders.page.tsx
+++ src/modules/order/pages/Orders.page.tsx
@@ -1,3 +1,3 @@
 export const OrdersPage: React.FC = () => {
-  return <div className="flex justify-center p-4 pt-8 text-xxl font-bold">This is the orders page.</div>;
+  return <div className="flex justify-center p-4 pt-8 text-xxl font-bold">This is the orders page</div>;
 };
