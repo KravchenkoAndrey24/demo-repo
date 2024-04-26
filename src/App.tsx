98a9150899edfd1f909928382dd162e76ffc1e9d
Andrii Kravchenko <yazaandrey@gmail.com>
2024-04-26 11:46:10 +0200

    fix: remove AS from typed env

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App: React.FC = () => {
   return (
     <Suspense fallback={<SuspenseLoader />}>
       <MsalProvider instance={msalInstance}>
-        <GoogleOAuthProvider clientId={TYPED_ENV.VITE_GOOGLE_CLIENT_ID as string}>
+        <GoogleOAuthProvider clientId={TYPED_ENV.VITE_GOOGLE_CLIENT_ID}>
           <RootModule />
         </GoogleOAuthProvider>
       </MsalProvider>
