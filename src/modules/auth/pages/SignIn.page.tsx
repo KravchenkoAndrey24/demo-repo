d2394a94e8ee1bb0f387f535ddfdb3b96e809977
Andrii Kravchenko <yazaandrey@gmail.com>
2024-08-16 21:18:51 +0200

    fix: remove auth with google and azure

--- src/modules/auth/pages/SignIn.page.tsx
+++ src/modules/auth/pages/SignIn.page.tsx
@@ -5,14 +5,14 @@ import { Button } from '@mui/material';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { LOCAL_STORAGE_KEYS, setLocalStorageValue } from '../../../utils/local-storage.utils';
 import { generateRandomId } from '../../../utils/utils';
-import { useGoogleLogin } from '@react-oauth/google';
-import { fetchUserGoogleInfo } from '../../../domain/auth/auth.api';
-import { useMsal } from '@azure/msal-react';
+// import { useGoogleLogin } from '@react-oauth/google';
+// import { fetchUserGoogleInfo } from '../../../domain/auth/auth.api';
+// import { useMsal } from '@azure/msal-react';
 import { Input } from '../../../components';
 
 export const SignInPage: React.FC = () => {
   const queryClient = useQueryClient();
-  const { instance } = useMsal();
+  // const { instance } = useMsal();
 
   const { formState, handleSubmit, register } = useForm<SignInSchema>({
     resolver: yupResolver(SignInSchema)
@@ -23,25 +23,25 @@ export const SignInPage: React.FC = () => {
     await queryClient.invalidateQueries();
   };
 
-  const googleLogin = useGoogleLogin({
-    onSuccess: async (tokenResponse) => {
-      const user = await fetchUserGoogleInfo(tokenResponse.access_token);
-      await saveUserData(user?.email || '');
-      return null;
-    }
-  });
+  // const googleLogin = useGoogleLogin({
+  //   onSuccess: async (tokenResponse) => {
+  //     const user = await fetchUserGoogleInfo(tokenResponse.access_token);
+  //     await saveUserData(user?.email || '');
+  //     return null;
+  //   }
+  // });
 
-  const { mutate: azureSingIn } = useMutation({
-    mutationFn: async () => {
-      try {
-        const res = await instance.loginPopup();
-        await saveUserData(res.account?.username || '');
-        return null;
-      } catch (error) {
-        console.error('Authentication error:', error);
-      }
-    }
-  });
+  // const { mutate: azureSingIn } = useMutation({
+  //   mutationFn: async () => {
+  //     try {
+  //       const res = await instance.loginPopup();
+  //       await saveUserData(res.account?.username || '');
+  //       return null;
+  //     } catch (error) {
+  //       console.error('Authentication error:', error);
+  //     }
+  //   }
+  // });
 
   const { mutate: singIn } = useMutation<null, Error, SignInSchema>({
     mutationFn: async (data) => {
@@ -67,12 +67,12 @@ export const SignInPage: React.FC = () => {
         <Button size="large" type="submit" variant="contained">
           Sign In
         </Button>
-        <Button onClick={() => googleLogin()} size="large" variant="contained">
+        {/* <Button onClick={() => googleLogin()} size="large" variant="contained">
           Sign In With Google
         </Button>
         <Button onClick={() => azureSingIn()} size="large" variant="contained">
           Sign In With Azure
-        </Button>
+        </Button> */}
         <div className="text-center text-xxs text-gray-600">
           If you do not have an account, we will automatically create one during login.
         </div>
